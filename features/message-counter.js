18aa6be430663875f396bee62c6883a678cd5039
Josh <joshdoody00@gmail.com>
2021-01-02 19:50:42 +1030

    finsished message counter

--- features/message-counter.js
+++ features/message-counter.js
@@ -3,10 +3,17 @@ const fs = require('fs')
 module.exports = (client) => {
     const channelId = '794803806402641940'
 
+    const interval = 5 * 60 * 1000
+    
     const stats = JSON.parse(fs.readFileSync(require.resolve('$data/stats.json')))
 
-    const updateMessages = (guild, stat) => {
-        const channel = guild.channels.cache.get(channelId)
+    const theGuild = client.guilds.cache.get('752781086882529280')
+
+    client.on('ready', () => {
+        setInterval(update(theGuild, stats), interval)
+    })
+
+    const updateMessages = (stat) => {
 
         var data = JSON.stringify(stat, null, 4)
         fs.writeFileSync(require.resolve('$data/stats.json'), data, 'utf8', (err) => {
@@ -15,22 +22,25 @@ module.exports = (client) => {
             }
         })
 
+    }
+
+    function update(guild, stats){
+        console.log('Message count updated')
+        const channel = guild.channels.cache.get(channelId)
         channel.setName(`Message Count: ${stats.messageCount.toLocaleString()}`)
     }
 
     client.on('message', message => {
         if (!message.author.bot){
             stats.messageCount = stats.messageCount + 1
-            const guild = client.guilds.cache.get('752781086882529280')
-            updateMessages(guild, stats)
+            updateMessages(stats)
         }
     })
 
     client.on('messageDelete', message => {
         if (!message.author.bot){
             stats.messageCount = stats.messageCount - 1
-            const guild = client.guilds.cache.get('752781086882529280')
-            updateMessages(guild, stats)
+            updateMessages(stats)
         }
     })
 
